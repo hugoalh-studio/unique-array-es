c89a276711f50c49b613a5549d684c6e5374cd24
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-17 18:09:26 +0800

    Improve test

--- mod.test.ts
+++ mod.test.ts
@@ -1,8 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/assert_equals.ts";
 import { assertThrows } from "https://deno.land/std@0.198.0/assert/assert_throws.ts";
 import uniqueArray from "./mod.ts";
-Deno.test("PASS", () => {
+Deno.test("0", () => {
 	assertEquals(uniqueArray([]), []);
+});
+Deno.test("1", () => {
 	assertEquals(uniqueArray([
 		{ foo: "bar" },
 		{ foo: "bar" },
@@ -11,10 +13,14 @@ Deno.test("PASS", () => {
 		{ foo: "bar" },
 		{ bar: "gaz" }
 	]);
+});
+Deno.test("2", () => {
 	assertEquals(uniqueArray([
 		{ type: { id: "_ETGENUS" } },
 		{ type: { id: "_ETGENUS" } }
 	]), [{ type: { id: "_ETGENUS" } }]);
+});
+Deno.test("3", () => {
 	assertEquals(uniqueArray([
 		{
 			id: "_1p7ZED73OF98VbT1SzSkjn",
@@ -43,22 +49,32 @@ Deno.test("PASS", () => {
 		},
 	]);
 });
-Deno.test("THROW", () => {
+Deno.test("4", () => {
 	assertThrows(() => {
 		uniqueArray(64);
 	});
+});
+Deno.test("5", () => {
 	assertThrows(() => {
 		uniqueArray(64n);
 	});
+});
+Deno.test("6", () => {
 	assertThrows(() => {
 		uniqueArray(false);
 	});
+});
+Deno.test("7", () => {
 	assertThrows(() => {
 		uniqueArray(undefined);
 	});
+});
+Deno.test("8", () => {
 	assertThrows(() => {
 		uniqueArray(null);
 	});
+});
+Deno.test("9", () => {
 	assertThrows(() => {
 		uniqueArray({ foo: "bar" });
 	});
