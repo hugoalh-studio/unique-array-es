3acb34e4ce23fea3b99332a66d5deaee7a82631a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-18 16:53:33 +0800

    Update resources

---
+++ mod.bench.ts
@@ -0,0 +1,11 @@
+import uniqueArray from "./mod.ts";
+Deno.bench({
+	name: "0",
+	fn: () => {
+		uniqueArray([
+			{ foo: "bar" },
+			{ foo: "bar" },
+			{ bar: "gaz" }
+		])
+	}
+});
--- mod.test.ts
+++ mod.test.ts
@@ -1,81 +1,111 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/assert_equals.ts";
 import { assertThrows } from "https://deno.land/std@0.198.0/assert/assert_throws.ts";
 import uniqueArray from "./mod.ts";
-Deno.test("0", () => {
-	assertEquals(uniqueArray([]), []);
+Deno.test({
+	name: "P-0",
+	fn: () => {
+		assertEquals(uniqueArray([]), []);
+	}
 });
-Deno.test("1", () => {
-	assertEquals(uniqueArray([
-		{ foo: "bar" },
-		{ foo: "bar" },
-		{ bar: "gaz" }
-	]), [
-		{ foo: "bar" },
-		{ bar: "gaz" }
-	]);
+Deno.test({
+	name: "P-1",
+	fn: () => {
+		assertEquals(uniqueArray([
+			{ foo: "bar" },
+			{ foo: "bar" },
+			{ bar: "gaz" }
+		]), [
+			{ foo: "bar" },
+			{ bar: "gaz" }
+		]);
+	}
 });
-Deno.test("2", () => {
-	assertEquals(uniqueArray([
-		{ type: { id: "_ETGENUS" } },
-		{ type: { id: "_ETGENUS" } }
-	]), [{ type: { id: "_ETGENUS" } }]);
+Deno.test({
+	name: "P-2",
+	fn: () => {
+		assertEquals(uniqueArray([
+			{ type: { id: "_ETGENUS" } },
+			{ type: { id: "_ETGENUS" } }
+		]), [{ type: { id: "_ETGENUS" } }]);
+	}
 });
-Deno.test("3", () => {
-	assertEquals(uniqueArray([
-		{
-			id: "_1p7ZED73OF98VbT1SzSkjn",
-			type: { id: "_ETGENUS" },
-			name: "Thuja",
-			friendlyId: "g-thuja",
-		},
-		{
-			id: "_567qzghxZmeQ9pw3q09bd3",
-			type: { id: "_ETGENUS" },
-			name: "Pinus",
-			friendlyId: "g-pinus",
-		},
-	]), [
-		{
-			id: "_1p7ZED73OF98VbT1SzSkjn",
-			type: { id: "_ETGENUS" },
-			name: "Thuja",
-			friendlyId: "g-thuja",
-		},
-		{
-			id: "_567qzghxZmeQ9pw3q09bd3",
-			type: { id: "_ETGENUS" },
-			name: "Pinus",
-			friendlyId: "g-pinus",
-		},
-	]);
+Deno.test({
+	name: "P-3",
+	fn: () => {
+		assertEquals(uniqueArray([
+			{
+				id: "_1p7ZED73OF98VbT1SzSkjn",
+				type: { id: "_ETGENUS" },
+				name: "Thuja",
+				friendlyId: "g-thuja",
+			},
+			{
+				id: "_567qzghxZmeQ9pw3q09bd3",
+				type: { id: "_ETGENUS" },
+				name: "Pinus",
+				friendlyId: "g-pinus",
+			},
+		]), [
+			{
+				id: "_1p7ZED73OF98VbT1SzSkjn",
+				type: { id: "_ETGENUS" },
+				name: "Thuja",
+				friendlyId: "g-thuja",
+			},
+			{
+				id: "_567qzghxZmeQ9pw3q09bd3",
+				type: { id: "_ETGENUS" },
+				name: "Pinus",
+				friendlyId: "g-pinus",
+			},
+		]);
+	}
 });
-Deno.test("4", () => {
-	assertThrows(() => {
-		uniqueArray(64);
-	});
+Deno.test({
+	name: "F-1",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray(64);
+		});
+	}
 });
-Deno.test("5", () => {
-	assertThrows(() => {
-		uniqueArray(64n);
-	});
+Deno.test({
+	name: "F-2",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray(64n);
+		});
+	}
 });
-Deno.test("6", () => {
-	assertThrows(() => {
-		uniqueArray(false);
-	});
+Deno.test({
+	name: "F-3",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray(false);
+		});
+	}
 });
-Deno.test("7", () => {
-	assertThrows(() => {
-		uniqueArray(undefined);
-	});
+Deno.test({
+	name: "F-4",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray(undefined);
+		});
+	}
 });
-Deno.test("8", () => {
-	assertThrows(() => {
-		uniqueArray(null);
-	});
+Deno.test({
+	name: "F-5",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray(null);
+		});
+	}
 });
-Deno.test("9", () => {
-	assertThrows(() => {
-		uniqueArray({ foo: "bar" });
-	});
+Deno.test({
+	name: "F-6",
+	fn: () => {
+		assertThrows(() => {
+			uniqueArray({ foo: "bar" });
+		});
+	}
 });
