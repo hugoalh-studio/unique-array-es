6cf1699208b7ea0516727589429e700698f60cdb
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-10-21 16:32:19 +0800

    Update dependenices

--- mod.test.ts
+++ mod.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import { assertEquals } from "https://deno.land/std@0.204.0/assert/assert_equals.ts";
 import { uniqueArray } from "./mod.ts";
 Deno.test("Empty", { permissions: "none" }, () => {
 	assertEquals(uniqueArray([]), []);
--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { equal } from "https://deno.land/std@0.203.0/assert/equal.ts";
+import { equal } from "https://deno.land/std@0.204.0/assert/equal.ts";
 /**
  * Return unique array elements without any duplicated elements by ignore their reference points.
  * @template {unknown} T
