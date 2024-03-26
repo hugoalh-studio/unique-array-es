9a4fc12b56493d124f3adb6c13bce1278bb107b6
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-03-26 16:22:07 +0800

    Update dependencies

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { equal } from "https://deno.land/std@0.219.1/assert/equal.ts";
+import { equal } from "https://deno.land/std@0.220.1/assert/equal.ts";
 /**
  * Return unique array elements without any duplicated elements by ignore their reference points.
  * @template {unknown} T
