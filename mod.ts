107e8b31098fe4fe6799e093810cba7b63ceb3c5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-03-29 10:47:41 +0800

    Update dependency "Deno Standard Library" (`deno/denoland:std`, `jsr:@std`) from v0.220.1 to v0.221.0

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { equal } from "https://deno.land/std@0.220.1/assert/equal.ts";
+import { equal } from "https://deno.land/std@0.221.0/assert/equal.ts";
 /**
  * Return unique array elements without any duplicated elements by ignore their reference points.
  * @template {unknown} T
