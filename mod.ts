05ad9b21f6fe94557f6b325e6bd9018efa63196d
hugoalh <hugoalh@gmail.com>
2024-12-04 17:37:54 +0800

    Add `@deprecated` on default import

--- mod.ts
+++ mod.ts
@@ -1,5 +1,8 @@
 export { isArrayUnique } from "./assert.ts";
 export {
+	/**
+	 * @deprecated This default import is deprecated, use named import with `uniqueArray` instead.
+	 */
 	default,
 	uniqueArray
 } from "./uniquify.ts";
